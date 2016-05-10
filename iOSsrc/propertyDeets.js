7f3f8ea7f1185ba25ca5bd163aba8b91e5582187
Michael Berber <mickberber@icloud.com>
2016-05-09 19:33:24 -0700

    init property deets

---
+++ iOSsrc/propertyDeets.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import React, { StyleSheet, Image, View, Text, Component } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 65
+  },
+  heading: {
+    backgroundColor: '#F8F8F8',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#DDDDDD'
+  },
+  image: {
+    width: 400,
+    height: 300
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    margin: 5,
+    color: '#656565'
+  },
+  title: {
+    fontSize: 20,
+    margin: 5,
+    color: '#48BBEC'
+  },
+  description: {
+    fontSize: 18,
+    margin: 5,
+    color: '#656565'
+  }
+});
